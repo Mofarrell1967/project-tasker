8778f42b656b45649699803520ac8ec5ef5512ed
Michael O'Farrell <mofarrell1967@gmail.com>
2020-01-18 10:43:13 +0000

    Updated app.py with initial app.route

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
+
+
+@app.route('/')
+def get_projects():
+    return render_template("projects.html", projects=mongo.db.projects.find())
+
+    
